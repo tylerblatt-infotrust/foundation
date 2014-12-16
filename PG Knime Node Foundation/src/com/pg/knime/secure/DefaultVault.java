cf5bf7633a0a8d3aa485103b6d79a0de4573390a
ckevinhill <hill.ck@pg.com>
2014-12-15 22:27:42 -0500

    Adding vault interface to separate credentials from implementation

---
+++ PG Knime Node Foundation/src/com/pg/knime/secure/DefaultVault.java
@@ -0,0 +1,41 @@
+package com.pg.knime.secure;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.lang.StringUtils;
+
+public class DefaultVault {
+
+	private Map<String, String> vault = new HashMap<String, String>();
+	
+	// TODO: Research at using char[] instead
+	protected void addKey(String key, String value) {
+		vault.put(key, value);
+	}
+	
+	public String getKey(String key) {
+		String value = vault.get(key);
+		if ( value == null ) return "";
+		
+		return value;
+	}
+	
+	protected String arrayToString( String... values ) {
+		return StringUtils.join(values, ",");
+	}
+	
+	public static DefaultVault getVault ( String vaultClass ) {
+		DefaultVault vault = new DefaultVault();
+		
+		try {
+			return (DefaultVault)Class.forName(vaultClass).newInstance();
+		}
+		catch ( Exception exc ) {
+			
+		}
+		
+		return vault;
+		
+	}
+}
